a7862ac10b008d85d98f0c5c935e82b2b1de513c
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-01-27 00:02:41 +0530

    Modified to Check File Exists Or Not program

---
+++ FileIOOperations/FileOperations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOOperations
+{
+    //Created Class
+    public class FileOperations
+    {
+        //Created File Exists Method
+        public static bool FileExists(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    Console.WriteLine("Given file exists");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Given file is not exists");
+                    return false;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return default;
+        }
+    }
+}
--- FileIOOperations/Program.cs
+++ FileIOOperations/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to File IO Operations");
+            Console.WriteLine("Please enter option given below");
+            Console.WriteLine("\n1:File Exists");
+            int option = Convert.ToInt32(Console.ReadLine());
+            string path = "C:\\Users\\hp\\source\\repos\\FileIOOperations\\FileIOOperations\\Program.cs";
+            //Switch case to call different method
+            switch (option)
+            {
+                case 1:
+                    FileOperations.FileExists(path);
+                    break;
+            }
         }
     }
 }
