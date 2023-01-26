462e49060b4686fcd6166cacf61fb9b839d0dbf7
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-01-27 01:34:27 +0530

    Modified to Xml Data Serialize program

---
+++ FileIOOperations/XmlDataOperation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace FileIOOperations
+{
+    //Created class
+    public class XmlDataOperation
+    {
+        public static string path = "C:\\Users\\hp\\source\\repos\\FileIOOperations\\FileIOOperations\\Sample.Txt";
+        //Created method for Xml Data Serialize
+        public static void XmlDataSerialize()
+        {
+            try
+            {
+                List<Contact> contact = new List<Contact>()
+                {
+                new Contact(){ID=1,Name="Sindhu",Address="Banglr"},
+                new Contact(){ID=2,Name="Haresh",Address="Mumbai"}
+                };
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Contact>));
+                StreamWriter sw = new StreamWriter(path);
+                xmlSerializer.Serialize(sw, contact);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
